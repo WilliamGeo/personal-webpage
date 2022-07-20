688b8e06bc51d59523c778ecb7360b0108e6e06a
William Georgeson <73065169+WilliamGeo@users.noreply.github.com>
2022-07-20 22:21:34 +0100

    Update

--- assets/js/additional.js
+++
@@ -1,5 +0,0 @@
-function getAge(dateString) {
-    var ageInMilliseconds = new Date() - new Date(dateString);
-    return Math.floor(ageInMilliseconds/1000/60/60/24/365); // convert to years
- }
- console.log(getAge('1997-04-23'));
